81202686465de94567dd1a466e81a43ecae5d731
Barbara Bothe <barbara.bothe@fau.de>
2025-11-03 17:49:43 +0100

    Versionsnummer

--- build/block/index.asset.php
+++ build/block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ac2bddab976626a04f8d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'b918309786750e66ef8f');
